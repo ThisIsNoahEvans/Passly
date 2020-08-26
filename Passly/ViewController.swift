21538517d4ce23e839f5d28f2b9c170e5db5197a
Noah Evans <noah@itsnoahevans.co.uk>
2020-08-26 18:05:10 +0100

    Removed pods, changed UI a bit, added Custom length

--- Passly/ViewController.swift
+++ Passly/ViewController.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+
+class ViewController: UIViewController{
 
     @IBOutlet weak var genButtonOutlet: UIButton!
-    @IBOutlet weak var labelOutlet: UILabel!
-    @IBOutlet weak var animateView: UIView!
+
+   
+    @IBOutlet weak var slider: UISlider!
     
+    var passwordLength: Int = 0
+    
+    
+    
+ 
+    
+  
     func getPassword (length: Int) -> String {
         let allowedChars = #"""
         abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
@@ -30,9 +40,18 @@ class ViewController: UIViewController {
         return randomString
     }
     
+
+    var checkView = SuccessCheck()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        labelOutlet.isHidden = true
+      
+        
+     //   checkView = SuccessCheck(frame: CGRect(x: 200, y: 200, width: 100, height: 100))
+        
+        // Add to the parent view
+        self.view.addSubview(checkView)
+
         self.genButtonOutlet.applyGradient(colors: [UIColor(named: "purple1")!, UIColor(named: "purple2")!])
         self.genButtonOutlet.layer.cornerCurve = .continuous
         self.genButtonOutlet.clipsToBounds = true
@@ -40,15 +59,82 @@ class ViewController: UIViewController {
     }
 
     @IBAction func generateButton(_ sender: Any) {
-        let password = getPassword(length: 150)
+        UIImpactFeedbackGenerator(style: .heavy).impactOccurred(intensity: 1.0)
+        let sliderLength: Int = Int(slider.value)
+        let password = getPassword(length: sliderLength)
         let pasteboard = UIPasteboard.general
         pasteboard.string = password
-        labelOutlet.isHidden = false
+        
+        // Animate the tick process
+      //  checkView.initWithTime(withDuration: 2, bgCcolor: .green, colorOfStroke: .white, widthOfTick: 10) {
+            //do additional work after completion
+        
+    }
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+        if 1...50 ~= slider.value {
+            UIImpactFeedbackGenerator(style: .light).impactOccurred(intensity: 0.2)
+        }
+        if 51...100 ~= slider.value {
+            UIImpactFeedbackGenerator(style: .light).impactOccurred(intensity: 0.3)
+        }
+        if 101...150 ~= slider.value {
+            UIImpactFeedbackGenerator(style: .light).impactOccurred(intensity: 0.4)
+        }
+        if 151...200 ~= slider.value {
+            UIImpactFeedbackGenerator(style: .light).impactOccurred(intensity: 0.5)
+        }
+        if 201...256 ~= slider.value {
+            UIImpactFeedbackGenerator(style: .light).impactOccurred(intensity: 0.6)
+        }
         
         
     }
     
+    @IBAction func customLength(_ sender: Any) {
+        
+        let ac = UIAlertController(title: "Custom Length", message: "If you're generating a very large password, the app may freeze for a second.", preferredStyle: .alert)
+        ac.addTextField()
+
+        let submitAction = UIAlertAction(title: "Generate", style: .default) { [self, unowned ac] _ in
+            
+            let answer = ac.textFields![0]
+            answer.keyboardType = .decimalPad
+
+            passwordLength = (answer.text! as NSString).integerValue
+            
+
+            
+            if passwordLength > 3000000 {
+                let tooBigAlert = UIAlertController(title: "Woah there!", message: "You can't generate passwords with more than 3,000,000 characters using Passly.", preferredStyle: .alert)
+                let tooBigOkay = UIAlertAction(title: "Okay!", style: .default, handler: nil)
+                
+                tooBigAlert.addAction(tooBigOkay)
+                present(tooBigAlert, animated: true, completion: nil)
+            }
+            else {
+                UIImpactFeedbackGenerator(style: .heavy).impactOccurred(intensity: 1.0)
+                let password = getPassword(length: passwordLength)
+                let pasteboard = UIPasteboard.general
+                pasteboard.string = password
+
+                
+            }
+            
+
+        }
+
+        ac.addAction(submitAction)
+
+        present(ac, animated: true) {
+
+            UIImpactFeedbackGenerator(style: .light).impactOccurred(intensity: 0.7)
+        }
+
+    }
 }
+
+
 
 extension UIView {
     @discardableResult
